3d056e27eb7e4b91c17c89ad450ee5cf1c298248
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2023-02-01 15:24:14 +0200

    Update index.tsx

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -68,7 +68,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             <form
               target="_blank"
               onSubmit={onSubmit}
-              action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+              action="https://formsubmit.co/75a19555b4381df54b9ccb4ed70fe32e"
               method="POST"
             >
               <input
